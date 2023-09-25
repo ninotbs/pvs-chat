a813b2faa09db9a11cb5b842bfba0483bd106642
ninotbs <nino_tbs@mac.com>
2023-09-25 23:03:15 +0200

    app.js created

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const http = require('http')
+const livereload = require('livereload')
+const connectLiveReload = require('connect-livereload')
+
+const app = express()
+const server = http.createServer(app)
+
+const env = process.env.NODE_ENV || 'development'
+if (env !== 'production') {
+  const liveReloadServer = livereload.createServer()
+  liveReloadServer.server.once('connection', () => {
+    setTimeout(() => {
+      liveReloadServer.refresh('/')
+    }, 100)
+  })
+  app.use(connectLiveReload)
+}
+
+(async function () {
+  const serverPort = process.env.PORT || 3000
+  server.listen(serverPort, () => {
+    console.log(
+      `Server started on port ${serverPort} as '${env}' Environment`
+    )
+  })
+})
